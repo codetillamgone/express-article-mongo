0d7d876444e456189407205f9834ea845329f2b6
Adebiyi David <adebiyi137@gmail.com>
2019-04-06 21:14:09 +0100

    Added styling and the logic for the create and read operation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose')
-
+const bodyParser = require('body-parser')
 //Connect to database
 mongoose.connect('mongodb://localhost/express-app-db')
 let db = mongoose.connection;
@@ -27,6 +27,17 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
 
+
+// Body Parser MiddleWare
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 //Home Route
 app.get('/', (req, res)=>{
 
@@ -45,14 +56,45 @@ app.get('/', (req, res)=>{
    
 })
 
+//Get Single Article
+app.get('/article/:id', (req, res)=>{
+    Article.findById(req.params.id, (err, article)=>{
+        res.render('article', 
+        {
+            article: article
+        } )
+    })
+})
+
 
 //Add Route
 app.get('/articles/add', (req, res)=>{
 
-    res.render('add_article', {
+    res.render('add_article', 
+    {
         title : "Add Article", 
         
     })
+})
+
+
+//Add Submot POST Route
+app.post('/articles/add', (req, res)=>{
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author
+    article.body = req.body.body
+
+    article.save((err)=>{
+        if(err){
+            console.log(err)
+
+        }
+        else{
+            res.redirect('/')
+        }
+    })
+    
 })
 
 //Start Server
