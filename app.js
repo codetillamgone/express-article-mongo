1377822075931ee82db331fdc558438b4e3e2635
Adebiyi David <adebiyi137@gmail.com>
2019-04-06 11:41:04 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path')
+const mongoose = require('mongoose')
+
+//Connect to database
+mongoose.connect('mongodb://localhost/express-app-db')
+let db = mongoose.connection;
+
+//Check Connection
+db.once('open', ()=>{
+    console.log('Connected to Mongodb');
+})
+
+//Check for DB errors
+db.on('error', (err)=>{
+    console.log(err)
+})
+
+//Init App
+const app = express();
+
+//Bring in Models
+let Article = require('./models/article');
+
+
+//Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+//Home Route
+app.get('/', (req, res)=>{
+
+    Article.find({}, (err, articles)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render('index', {
+                title : "Articles", 
+                articles : articles
+            });
+        }
+   
+    })
+   
+})
+
+
+//Add Route
+app.get('/articles/add', (req, res)=>{
+
+    res.render('add_article', {
+        title : "Add Article", 
+        
+    })
+})
+
+//Start Server
+app.listen(3000, ()=>(console.log("Server started on port 3000...")))
