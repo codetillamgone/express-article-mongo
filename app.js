6993342aaf0ad36b4cbecefa97df111926e61091
Adebiyi David <adebiyi137@gmail.com>
2019-04-08 13:48:08 +0100

    implemented login and form validation with express

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const expressValidator = require('express-validator')
+const {check, validationResult} = require('express-validator/check')
 const flash = require('connect-flash')
 const session = require('express-session')
 
@@ -76,91 +76,9 @@ app.get('/', (req, res)=>{
    
 })
 
-//Get Single Article
-app.get('/article/:id', (req, res)=>{
-    Article.findById(req.params.id, (err, article)=>{
-        res.render('article', 
-        {
-            article: article
-        } )
-    })
-})
-
-//Load Edit Form
-app.get('/article/edit/:id', (req, res) => {
-    Article.findById(req.params.id, (err, article) =>{
-        res.render('edit_article', {
-            title: 'Edit Article', 
-            article : article
-        })
-    })
-})
-
-
-//Add Route
-app.get('/articles/add', (req, res)=>{
-
-    res.render('add_article', 
-    {
-        title : "Add Article", 
-        
-    })
-})
-
-
-//Add Submit POST Route
-app.post('/articles/add', (req, res)=>{
-    let article = new Article();
-    article.title = req.body.title;
-    article.author = req.body.author
-    article.body = req.body.body
-
-    article.save((err)=>{
-        if(err){
-            console.log(err)
-
-        }
-        else{
-            req.flash('success', 'Article Added')
-            res.redirect('/')
-        }
-    })
-    
-})
-
-//Update Submit POST Route
-app.post('/article/edit/:id', (req, res) =>{
-    let article = {};
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
-
-    let query = {_id: req.params.id}
-
-    Article.update(query, article, (err)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            req.flash('success', 'Article Updated')
-            res.redirect('/')
-        }
-    })
-})
-
-//Delete Article Route
-app.delete('/article/:id', (req, res)=>{
-    let query = {_id: req.params.id}
-
-    Article.deleteOne(query, (err)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.send('Success')
-        } 
-    })
-})
+//Route files
+let articles = require('./routes/articles')
+app.use('/articles', articles)
 
 //Start Server
 app.listen(3000, ()=>(console.log("Server started on port 3000...")))
---
+++ routes/articles.js
@@ -0,0 +1,124 @@
+const express = require('express')
+const router = express.Router()
+const {check, validationResult} = require('express-validator/check')
+
+
+//Bring in Models
+let Article = require('../models/article');
+
+
+
+
+//Load Edit Form
+router.get('/edit/:id', (req, res) => {
+    Article.findById(req.params.id, (err, article) =>{
+        res.render('edit_article', {
+            title: 'Edit Article', 
+            article : article
+        })
+    })
+})
+
+
+//Add Route
+router.get('/add', (req, res)=>{
+
+    res.render('add_article', 
+    {
+        title : "Add Article", 
+        
+    })
+})
+
+
+//Add Submit POST Route
+router.post('/add',[
+    check('title').isLength({min:1}),
+    check('author').isLength({min:1}),
+    check('body').isLength({min: 1}),
+    
+], (req, res)=>{
+    
+
+    
+    //get Errors
+    const errors = validationResult(req)
+
+    if(!errors.isEmpty()){
+        
+        console.log(errors)
+        console.log(errors.isEmpty())
+        console.log(errors.array())
+        
+        res.render('add_article', {
+            title: 'Add Article',
+            errors: errors.array()
+            
+        })
+    } else {
+        let article = new Article();
+        article.title = req.body.title;
+        article.author = req.body.author
+        article.body = req.body.body
+
+        article.save((err)=>{
+            if(err){
+                console.log(err)
+
+            }
+            else{
+                req.flash('success', 'Article Added')
+                res.redirect('/')
+            }
+        })
+    }
+
+    
+    
+})
+
+//Update Submit POST Route
+router.post('/edit/:id', (req, res) =>{
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id: req.params.id}
+
+    Article.update(query, article, (err)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            req.flash('success', 'Article Updated')
+            res.redirect('/')
+        }
+    })
+})
+
+//Delete Article Route
+router.delete('/:id', (req, res)=>{
+    let query = {_id: req.params.id}
+
+    Article.deleteOne(query, (err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send('Success')
+        } 
+    })
+})
+
+//Get Single Article
+router.get('/:id', (req, res)=>{
+    Article.findById(req.params.id, (err, article)=>{
+        res.render('article', 
+        {
+            article: article
+        } )
+    })
+})
+
+module.exports = router;
